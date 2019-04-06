aa9f456d20ed6d02cc4158873fd3cfc46cf87f97
michael <mnzube@gmail.com>
2019-04-06 17:01:38 +0200

    [Feature#165163745] Enables user sign up

--- server/index.js
+++ server/index.js
@@ -1,5 +1,5 @@
 import express from 'express';
-
+import users from "./routes/users"
 const app = express();
 
 app.use(express.urlencoded({extended: true}));
@@ -9,7 +9,7 @@ app.get('/', (req, res) => {
     res.send('Welcome to Banka');
 })
 
-
+app.use("/api/v1",users)
 const port = process.env.PORT  || 3000;
 
 app.listen(port, () => console.log(`Listening on port ${port}`) );
