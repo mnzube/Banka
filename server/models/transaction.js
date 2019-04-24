c877a6beb15abfcb629b34ea55168f3d07d085a3
michael <mnzube@gmail.com>
2019-04-24 09:48:30 +0200

    [start#165530148] Implements reviews and fixes bugs

--- server/models/transaction.js
+++ server/models/transaction.js
@@ -1,25 +1,3 @@
-import uuid from "uuid";
+import pool from "../config/database";
 
-class Transaction {
-  constructor() {
-    this.transaction = [];
-  }
 
-  //creates an account
-  create(data) {
-    const newTransaction = {
-      id: uuid.v4(),
-      accountNumber: data.accountNumber,
-      cashier: data.cashier,
-      type: data.type,
-      amount: data.amount,
-      oldBalance: data.oldBalance,
-      newBalance: data.newBalance,
-      createdOn: new Date()
-    };
-    this.transaction.push(newTransaction);
-    return newTransaction;
-  }
-}
-
-export default new Transaction();
